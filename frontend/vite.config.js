bac18d1ac72d6d104b82763053295f93481bf22d
Ansh-Majumdar619 <anshmajumdar619@gmail.com>
2025-06-23 16:50:01 +0530

    Fix: add vite plugin to include _redirects in build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,32 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '' // this copies it to the root of dist/
+        }
+      ]
+    })
+  ]
 })
