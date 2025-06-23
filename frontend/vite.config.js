90d86249732f300d802f264fb4b4afdc8d38b3d5
Ansh-Majumdar619 <anshmajumdar619@gmail.com>
2025-06-23 17:01:23 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
-
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '' // this copies it to the root of dist/
-        }
-      ]
-    })
-  ]
+  plugins: [react(), tailwindcss()],
 })
